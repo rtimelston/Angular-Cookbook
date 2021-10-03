ddd5b58d1d169b904e58f9aef699a08f9b17be1c
Tim Elston <elstonr@pdx.odshp.com>
2021-10-03 15:27:05 -0700

    Chapter 1 Using ngOnChanges to intercept input property changes

--- chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss']
 })
-export class VcLogsComponent implements OnInit {
+export class VcLogsComponent implements OnInit, OnChanges {
   @Input() vName;
+  logs: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /* ngOnChanges is one of the many life cycle hooks Angular provides out of the box. It triggers even before the
+     ngOnInit hook. So, you get the initial values in the first call and the updated values later on. Whenever any of
+     the inputs change, the ngOnChanges callback is triggered with SimpleChanges and you can get the previous value,
+     the current value, and a Boolean representing whether this is the first change to the input (that is, the initial
+     value). When we update the value of the vName input in the parent, ngOnChanges gets called with the updated value.
+     Then, based on the situation, we add an appropriate log into our logs array and display it on the UI.
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    const currValue = changes.vName.currentValue;
+    if(changes.vName.isFirstChange()) {
+      this.logs.push(`initial version is ${currValue.trim()}`);
+    } else {
+      this.logs.push(`version changed to ${currValue.trim()}`)
+    }
   }
 
 }
