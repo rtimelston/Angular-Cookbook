021403db131cbfafa6b171a28d87850418b7a463
Tim Elston <elstonr@pdx.odshp.com>
2021-10-11 21:30:13 -0700

    Chapter 2 Creating a directive to calculate the rad time for articles

---
+++ chapter02/start_here/ng-read-time-directive/src/app/directives/read-time.directive.ts
@@ -0,0 +1,39 @@
+import { Directive, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+export interface ReadTimeConfig {
+  wordsPerMinute: number;
+}
+@Directive({
+  selector: '[appReadTime]'
+})
+export class ReadTimeDirective implements OnInit {
+  configuration: ReadTimeConfig = {
+    wordsPerMinute: 200
+  }
+  @Output() readTimeCalculated = new EventEmitter<string>();
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    const text = this.el.nativeElement.textContent;
+    const time = this.calculateReadTime(text);
+    const timeStr = this.createTimeString(time);
+    this.readTimeCalculated.emit(timeStr);
+  }
+
+  calculateReadTime(text: string): number {
+    const wordsCount = text.split(/\s+/g).length;
+    const minutes = wordsCount / this.configuration.wordsPerMinute;
+    return Math.ceil(minutes);
+  }
+
+  createTimeString(timeInMinutes: number) {
+    if(timeInMinutes === 1) {
+      return '1 minute';
+    } else if (timeInMinutes < 1) {
+      return '< 1 minute';
+    } else {
+      return `${timeInMinutes} minutes`;
+    }
+  }
+}
